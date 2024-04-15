7edcaa399d27410a54860ef66366d31eabe94056
Lightninbolt986 <68819260+Lightninbolt986@users.noreply.github.com>
2024-04-15 14:04:02 +0530

    Initial commit

---
+++ motion.gd
@@ -0,0 +1,26 @@
+extends AnimatedSprite2D
+
+var speed = 200
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	
+	var direction = Vector2.ZERO
+	if Input.is_action_pressed("ui_left"):
+		direction.x = -1
+	if Input.is_action_pressed("ui_right"):
+		direction.x = 1
+	if Input.is_action_pressed("ui_up"):
+		direction.y = -1
+	if Input.is_action_pressed("ui_down"):
+		direction.y = 1
+	direction = direction.normalized()
+	position += direction * speed * delta
+	rotation = direction.angle()
+	if direction.length() > 0:
+		play()
+	else:
+		stop()
