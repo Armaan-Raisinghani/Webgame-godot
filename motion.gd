1ce804c2ce33fc19782458a7e8a5bde6191300e1
Lightninbolt986 <68819260+Lightninbolt986@users.noreply.github.com>
2024-04-16 12:32:05 +0530

    move doors out of timemap to add custom data

--- motion.gd
+++ motion.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 var speed = 500
 @onready var child = $AnimatedSprite2D
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	pass # Replace with function body.
 
