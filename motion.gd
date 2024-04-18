4b665c8b91c8b17c36e0d48401865f786cd634b1
Lightninbolt986 <68819260+Lightninbolt986@users.noreply.github.com>
2024-04-18 21:43:02 +0530

    added working quest system using questify asset

--- motion.gd
+++ motion.gd
@@ -2,8 +2,20 @@ extends CharacterBody2D
 
 var speed = 500
 @onready var child = $AnimatedSprite2D
+@onready var data_manager = get_node("/root/Game/DataManager")
+@export var quest: QuestResource
+
 signal load_interior(location: int, player: Node)
+
 var enterable
+func _ready() -> void:
+	var instance := quest.instantiate()
+	Questify.start_quest(instance)
+	Questify.quest_completed.connect(func(_quest: QuestResource):
+		print('quest completed hell yea'))
+	Questify.quest_objective_completed.connect(func(_quest: QuestResource, _objective: QuestObjective):
+		print('we did it we did it')
+	)
 func _process(delta):
 	var direction = Vector2.ZERO
 	if Input.is_action_pressed("ui_left"):
@@ -29,9 +41,12 @@ func _on_area_2d_area_shape_entered(_area_rid, area, _area_shape_index, _local_s
 		if area.get_parent().get_parent().has_meta("DoorLocation"):
 			var location = area.get_parent().get_parent().get_meta("DoorLocation")
 			emit_signal("load_interior", location, self)
-			print(location)
+		
 			enterable = false
 	
 func _on_area_2d_area_shape_exited(_area_rid, area, _area_shape_index, _local_shape_index):
 	if area.collision_layer == 2:
 		enterable = true
+
+func _on_area_2d_mouse_entered():
+	data_manager.set_value("go", true)
